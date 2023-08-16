cd28474404f0007930973ed8635b026eda25b50b
caioarnonii <caio.bernardo@sptech.school>
2023-08-16 09:38:28 -0300

    Site

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const IP = require('ip');
+const { spawn } = require("child_process");
+const PORTA = 3333;
+
+var app = express();
+
+var indexRouter = require("./src/routes/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+
+const ipAddress = IP.address();
+
+const linkServer = `http://${ipAddress}:${PORTA}`;
+
+app.listen(PORTA, function () {
+    console.log(`Servidor Rodando Em:  ${linkServer}`);
+});
+
+const curl = spawn("curl", [`qrenco.de/${linkServer}`]);
+
+curl.stdout.on("data", data => {
+    console.log(`qrcode:\n${data}`);
+});
