350762f17bbb75dba577cf3c0466d8a1dfd6ee1c
lucasrocha2704 <lucas.rocha@sptech.school>
2023-08-21 21:36:46 -0300

    Responsividade tela de perfil

---
+++ public/assets/js/perfil.js
@@ -0,0 +1,96 @@
+var aparece = false;
+
+function mostrarControle() {
+
+    if (!aparece) {
+        controle.style = "width: 100%;";
+        hamburguer.style = "width: 60%;";
+        nome.style = "display: block;";
+        interacao.style = "display: flex;";
+        dadosGerais.style = "width: 0%;";
+
+        aparece = true
+    } else {
+        controle.style = "width: 10%";
+        hamburguer.style = "width: 100%;";
+        nome.style = "display: none;";
+        interacao.style = "display: none;";
+        dadosGerais.style = "width: 90%;";
+
+    }
+}
+
+function dadosCadastro() {
+
+    if (aparece) {
+        controle.style = "width: 10%";
+        hamburguer.style = "width: 100%;";
+        nome.style = "display: none;";
+        interacao.style = "display: none;";
+        dadosGerais.style = "width: 90%;";
+
+        dCadastro.style ="display: flex;";
+        dCarro.style ="display: none;";
+        confgCarro.style ="display: none;";
+
+        aparece = false
+    } else {
+        dCadastro.style ="display: flex;";
+        dCarro.style ="display: none;";
+        confgCarro.style ="display: none;";
+    }
+}
+
+function dadosCarro() {
+
+    if (aparece) {
+        controle.style = "width: 10%";
+        hamburguer.style = "width: 100%;";
+        nome.style = "display: none;";
+        interacao.style = "display: none;";
+        dadosGerais.style = "width: 90%;";
+
+        dCadastro.style ="display: none;";
+        dCarro.style ="display: flex;";
+        confgCarro.style ="display: none;";
+
+        aparece = false
+    } else {
+        dCadastro.style ="display: none;";
+        dCarro.style ="display: flex;";
+        confgCarro.style ="display: none;";
+
+    }
+}
+
+function confCarro() {
+
+    if (aparece) {
+        controle.style = "width: 10%";
+        hamburguer.style = "width: 100%;";
+        nome.style = "display: none;";
+        interacao.style = "display: none;";
+        dadosGerais.style = "width: 90%;";
+
+        dCadastro.style ="display: none;";
+        dCarro.style ="display: none;";
+        confgCarro.style ="display: flex;";
+
+        aparece = false
+    } else {
+        dCadastro.style ="display: none;";
+        dCarro.style ="display: none;";
+        confgCarro.style ="display: flex;";
+
+    }
+}
+
+function inicio() {
+
+    window.location = "index.html";
+}
+
+// function sair() {
+
+
+// }
