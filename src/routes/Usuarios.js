a636c0e6de3b58de1981270f4a3958def06455bc
KevynRochaAl <kevyn.alves@sptech.school>
2023-08-22 12:17:36 -0300

    Criando as rotas do login

---
+++ src/routes/Usuarios.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.entrar(req, res);
+});
+
+module.exports = router;
