1d1b4d106725fc82e58a3a6900a1128c777d7839
caioarnonii <caio.bernardo@sptech.school>
2023-08-25 09:05:04 -0300

    BackEnd cadastroADM v2

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,12 +1,12 @@
 var database = require("../database/config");
 
-function cadastrar(nome, email, senha, empresaId) {
+function cadastrar(nome, email, cpf, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${empresaId}');
+        INSERT INTO Usuario (nome, email, senha, cpf) VALUES ('${nome}', '${email}', '${senha}', '${cpf}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -23,5 +23,5 @@ function entrar(email,senha){
 
 module.exports={
     entrar,
-    cadastrar,
+    cadastrar
 }
