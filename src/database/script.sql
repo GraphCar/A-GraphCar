97417858c736c002ee71a7274aa5e851315bfbb5
Thigos <dev.findviewbyid@gmail.com>
2023-08-29 23:42:24 -0300

    Back Atualizado

--- src/database/script.sql
+++ src/database/script.sql
@@ -1,67 +1,101 @@
--- SQLBook: Code
--- Active: 1692980043447@@127.0.0.1@3306@graphcar
+-- Active: 1679521204876@@127.0.0.1@3306@bd_smfp
 DELETE FROM mysql.user where user = 'GraphUser';
 
 CREATE USER 'GraphUser'@'%' IDENTIFIED BY 'Graph2023';
-GRANT ALL PRIVILEGES ON Graph.* TO 'GraphUser'@'%';
+GRANT ALL PRIVILEGES ON GraphCar.* TO 'GraphUser'@'%';
+FLUSH PRIVILEGES;
 
 DROP DATABASE IF EXISTS GraphCar;
 CREATE DATABASE GraphCar;
 USE GraphCar;
 
 CREATE TABLE Usuario(
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
-    email VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
     senha VARCHAR(64),
-    cpf CHAR (11),
+    cpf CHAR (11) UNIQUE,
+    foto VARCHAR(100), 
     adm TINYINT
 );
 
-
 CREATE TABLE ModeloCarro(
-    idCarro INT PRIMARY KEY AUTO_INCREMENT,
-    modelo VARCHAR(30),
-    versaoSoftware VARCHAR(60)
+	idModelo INT PRIMARY KEY AUTO_INCREMENT,
+    Modelo VARCHAR(30),
+    VersaoSoftware VARCHAR(60)
 );
 
 CREATE TABLE Carro(
-    idCarro INT PRIMARY KEY AUTO_INCREMENT,
-    placa VARCHAR(15),
-    fkUsuario INT,
+	idCarro INT PRIMARY KEY AUTO_INCREMENT,
+    Placa VARCHAR(15) UNIQUE,
+	fkUsuario INT,
     fkModelo INT,
     CONSTRAINT fhkUsuario FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario),
-    CONSTRAINT fhkModelo FOREIGN KEY (fkModelo) REFERENCES ModeloCarro(idCarro)
+    CONSTRAINT fhkModelo FOREIGN KEY (fkModelo) REFERENCES ModeloCarro(idModelo)
 );
 
 CREATE TABLE Componentes(
-    idComponentes INT PRIMARY KEY AUTO_INCREMENT,
-    nomeComponente VARCHAR(15),
-    versaoDriver VARCHAR(15)
+	idComponentes INT PRIMARY KEY AUTO_INCREMENT,
+    NomeComponente VARCHAR(10),
+    VersaoDriver VARCHAR(15),
+    fkModelo INT,
+    FOREIGN KEY (fkModelo) REFERENCES ModeloCarro(idModelo)
 );
 
 CREATE TABLE Dados(
-    idDados INT PRIMARY KEY AUTO_INCREMENT,
-    temperatura DECIMAL(5,2),
-    voltagem DECIMAL(5,2),
-    memoria DECIMAL(7,2),
-    utilizacao INT,
-    DVSEnabled TINYINT,
+	idDados INT PRIMARY KEY AUTO_INCREMENT,
     dado FLOAT,
+    medida VARCHAR(10),
     dateDado DATETIME,
     fkCarro INT,
-    fkMedida INT,
     fkComponentes INT,
     CONSTRAINT fhkCarro FOREIGN KEY (fkCarro) REFERENCES Carro(idCarro),
     CONSTRAINT fhkComponentes FOREIGN KEY (fkComponentes) REFERENCES Componentes(idComponentes)
 );
+
+DELIMITER //
+CREATE PROCEDURE CADASTRAR_MOTORISTA(IN 
+
+	US_NOME VARCHAR(50), 
+    US_EMAIL VARCHAR(100), 
+    US_SENHA VARCHAR(64), 
+	US_CPF VARCHAR(11),
+    US_ADM TINYINT, 
+    C_PLACA VARCHAR(15), 
+    MC_MODELO VARCHAR(30)
+    
+    ) BEGIN 
+	INSERT INTO usuario (nome, email, senha, CPF, adm)
+	VALUES ( us_nome, us_email, us_senha, us_CPF, us_adm);
+    INSERT INTO ModeloCarro (Modelo)
+    VALUES (mc_modelo);
+	INSERT INTO Carro (Placa , fkUsuario, fkModelo)
+	VALUES ( c_placa,
+    (SELECT idUsuario FROM usuario WHERE email = us_email),
+    (SELECT idModelo FROM ModeloCarro WHERE idModelo = (SELECT idUsuario FROM usuario WHERE email = us_email)));
+	END// 
+DELIMITER ;
+
 INSERT INTO Usuario (nome, email, senha, cpf, adm) values ('ADM', 'admin@graphcar.com', '$2b$10$M/CbWCDYZcYYDnTUs1nfPOu/U665hzfQDSBucm56MxAy4ldau2YAi', '000', 3);
 
 INSERT INTO Componentes (idComponentes, nomeComponente) VALUES (NULL, "CPU");
-INSERT INTO Componentes (idComponentes, nomeComponente) VALUES (NULL, "Memória RAM");
+INSERT INTO Componentes (idComponentes, nomeComponente) VALUES (NULL, "RAM");
 INSERT INTO Componentes (idComponentes, nomeComponente) VALUES (NULL, "Disco");
 
-SELECT * from Usuario;
+-- SELECT * FROM usuario;
+-- SELECT * FROM carro;
+-- SELECT * FROM modeloCarro;
+
+-- DELETE from usuario WHERE idUsuario < 14 ;
+-- DELETE from ModeloCarro WHERE idCarro < 14 ;
+-- DELETE from Carro WHERE idCarro < 14;
+
+-- SELECT *
+--        FROM usuario u JOIN Carro c JOIN ModeloCarro mc on c.idCarro = mc.idCarro AND idUsuario = c.idCarro;
+
+-- Exemplo de call da procedure
+-- CALL cadastrar_motorista 
+-- ('lucas', 'lucas@gmail.com', 'lucas123', '55555555555', 'bbb9999', 'model X', 1);
+
 select * from Dados;
-select * from Medida;
 select * from Componentes;
