1e9c28e3722d459a6503399986bcbccadde0825e
Thigos <dev.findviewbyid@gmail.com>
2023-09-05 10:51:13 -0300

    Mask CPF

--- public/assets/js/cadastroMotorista.js
+++ public/assets/js/cadastroMotorista.js
@@ -7,7 +7,7 @@ if (planoVar < 1) {
 function cadastrar() {
     // Card 1
     var nomeVar = ipt_nome.value;
-    var cpfVar = ipt_cpf.value;
+    var cpfVar = ipt_cpf.value.replace('.', '').replace('-', '');
     var modeloVar = ipt_modelo.value;
     var placaVar = ipt_placa.value;
     var emailVar = ipt_email.value;
