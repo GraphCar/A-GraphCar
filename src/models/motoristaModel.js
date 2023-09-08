715c440054fd6c9092120c8828a929fa7f4a1b90
lucasrocha2704 <lucas.rocha@sptech.school>
2023-09-08 02:10:03 -0300

    correção cadastro motorista

--- src/models/motoristaModel.js
+++ src/models/motoristaModel.js
@@ -10,6 +10,16 @@ function cadastrarMotorista(nome, email, senha, cpf, plano, placa , modelo) {
     return database.executar(instrucao);
 }
 
+
+function mostrarModelos() {
+    console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirPerfil()");
+    var instrucao = `
+    SELECT * FROM modelocarro;
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+}
 module.exports={
-    cadastrarMotorista
+    cadastrarMotorista,
+    mostrarModelos
 }
--- src/routes/Motorista.js
+++ src/routes/Motorista.js
@@ -7,4 +7,8 @@ router.post("/cadastrarMotorista", function (req, res) {
     motoristaController.cadastrarMotorista(req, res);
 });
 
+router.get("/mostrarModelos", function (req, res) {
+    motoristaController.mostrarModelos(req, res);
+});
+
 module.exports = router;
